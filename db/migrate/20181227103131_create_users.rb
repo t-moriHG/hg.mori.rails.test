3a6e6ac7f85ac373470bf038dbc5fd82410aa481
t-moriHG <t-mori@hg.locopo-jp.com>
2018-12-27 20:47:06 +0900

    userテーブル作成

---
+++ db/migrate/20181227103131_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name,              limit: 255,   null: false
+      t.string :password_digest,   limit: 255,   null: false
+      t.string :remember_token,    limit: 255,   null: false
+      t.boolean :delete_flag,      limit: 255,   default: false
+      t.datetime :created_at,                    null: false
+      t.datetime :updated_at,                    null: false
+    end
+  end
+end
