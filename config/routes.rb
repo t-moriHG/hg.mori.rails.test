e5199de07d3d6d2fea186ab9560aef4a44433837
t-mori2015 <t-mori@hg.locopo-jp.com>
2018-12-28 00:21:58 +0900

    アカウント作成画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # アカウント作成
+  get     'account',   to: 'users#new'
+  post    'account',   to: 'users#create'
   # ログイン / ログアウト
   get     'login',   to: 'sessions#new'
   post    'login',   to: 'sessions#create'
