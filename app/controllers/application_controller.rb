17447515a78293d62ce0c8ed953011d3e3d23d9f
t-moriHG <t-mori@hg.locopo-jp.com>
2018-12-28 14:53:33 +0900

    Basic認証追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  #before_action :basic
+
+  private
+  def basic
+    authenticate_or_request_with_http_basic do |name, password|
+      name == "test" && password == "123456"
+    end
+  end
 end
